f03363a176f75faa817ea9a57b980cdd2a0cffd3
mohammad n nimer <139014861+mohammednnimer@users.noreply.github.com>
2024-03-28 01:05:54 -0700

    Update LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -1,4 +1,360 @@
-package dfk;
+package lab4;
+//that make in lab its nee some idyt
+public class curserArray<T extends Comparable<T>> {
+
+	CNode<T> newnode[];
+
+	public curserArray(int x) {
+		inttilized(x);
+
+	}
+
+	public void inttilized(int n) {
+		newnode = new CNode[n];
+		for (int i = 0; i <= newnode.length - 2; i++) {
+
+			newnode[i] = new CNode(null, i + 1);
+
+		}
+
+		newnode[n - 1] = new CNode(null, 0);
+
+	}
+
+	public int malloc() {
+		int p = newnode[0].next;
+		newnode[0].next = newnode[p].next;
+		return p;
+	}
+
+	public void free(int p) {
+		if (p > newnode.length) {
+			return;
+		}
+
+		newnode[p] = new CNode<>(null, newnode[0].next);
+		newnode[0].next = p;
+
+	}
+
+	public int creartlist() {
+		int n = malloc();
+		if (n != 0) {
+			newnode[n] = new CNode("domain", 0);
+			return n;
+
+		}
+		System.out.println("sorry the list is full");
+		return 0;
+	}
+
+	public boolean isnull(int l) {
+
+		return newnode[l].data == null;
+
+	}
+
+	public void insertFirst(T data, int l) {
+		if (isnull(l))
+			return;
+
+		int p = malloc();
+		if (p != 0) {
+			newnode[p] = new CNode<>(data, newnode[l].next);
+
+			newnode[l].next = p;
+
+		} else {
+			System.out.println("full");
+		}
+
+	}
+
+	public void tracverce(int n) {
+		
+		System.out.println();
+		if (isnull(n)) {
+			return;
+
+		}
+
+		int l = newnode[n].next;
+		while (l != 0) {
+
+			System.out.print(newnode[l].data + "-->");
+			l = newnode[l].next;
+		}
+
+	}
+
+	public void insertlast(T data, int l) {
+		int p = malloc();
+		if (p != 0) {
+
+			newnode[p] = new CNode<>(data, p);
+			int last = l;
+			while (newnode[last].next != 0) {
+				last = newnode[last].next;
+			}
+			newnode[p].next = newnode[last].next;
+			newnode[last].next = p;
+
+		} else {
+			System.out.println("sorry the arrayis full");
+		}
+
+	}
+	public void marge(int l,int m)
+	{
+		if(!is_Empty(l)&&!is_Empty(m))
+		{
+			l=newnode[l].next;
+			while (newnode[l].next!=0) {
+				l=newnode[l].next;
+			}
+			
+			newnode[l].next=newnode[m].next;
+			free(m);
+			
+			
+			
+			
+			
+		}
+		
+		
+		
+	}
+
+	public void insert_last_rec(T data, int n) {
+		int p = malloc();
+		if (p != 0) {
+
+			insert_last(data, n, p);
+
+		}
+
+	}
+	public boolean is_Empty(int l)
+	{
+		if(newnode[l].next==0)
+			return true;
+		return false;
+	}
+	
+	public void clear(int l)
+	{
+		while (!is_Empty(l)) {
+			int p=newnode[l].next;
+			
+			newnode[l].next=newnode[p].next;
+			free(p);
+			
+			
+			
+		}
+		
+		
+//		int curr=0;
+//		while (newnode[l].next!=0) {
+//			int p=newnode[l].next;
+//			
+//			newnode[l].next=newnode[p].next;
+//	free(p);
+//	
+//		
+//	
+//		
+//		}
+		
+	}
+	private void insert_last(T data, int n, int p) {
+
+		if (newnode[n].next != 0) {
+			n = newnode[n].next;
+			insert_last(data, n, p);
+
+		} else if (newnode[n].next == 0) {
+
+			newnode[p] = new CNode<>(data, p);
+			newnode[p].next = 0;
+			newnode[n].next = p;
+		}
+
+	}
+
+	public T Delet_At_last(int l) {
+		if (newnode[l].next != 0) {
+			int last = 0;
+
+			while (newnode[newnode[l].next].next != 0) {
+
+				l = newnode[l].next;
+			}
+			T data = newnode[newnode[l].next].data;
+			free(newnode[l].next);
+
+			newnode[l].next = 0;
+
+			return data;
+
+		}
+		return null;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+	public int length(int n) {
+		int curr = 0;
+		while (newnode[n].next != 0) {
+
+			curr++;
+			n = newnode[n].next;
+
+		}
+		return curr;
+
+	}
+
+	public int length_rec(int n) {
+
+		return length_rec(n, 0);
+
+	}
+
+	private int length_rec(int n, int i) {
+
+		if (newnode[n].next == 0) {
+			return i;
+		}
+		return length_rec(newnode[n].next, ++i);
+
+	}
+
+	public T deletFirst(int l) {
+		if (newnode[l] != null) {
+			int x = newnode[l].next;
+			newnode[l].next = newnode[x].next;
+			T data = newnode[x].data;
+			free(x);
+			newnode[x].data = null;
+			return data;
+
+		}
+
+		return null;
+	}
+
+	public void Insert_Sorting(T data, int l) {
+		int p = malloc();
+		if (p != 0) {
+			if (newnode[l] != null) {
+
+				newnode[p] = new CNode<>(data, 0);
+				int first = newnode[l].next;
+
+				if (newnode[first].data == null) {
+
+					newnode[p].next = 0;
+
+					newnode[l].next = p;
+
+					return;
+
+				}
+
+				int last = first;
+				if (newnode[first].data.compareTo(newnode[p].data) > 0) {
+
+					newnode[p].next = newnode[l].next;
+					newnode[l].next = p;
+
+					return;
+
+				}
+				while (newnode[first].data.compareTo(newnode[p].data) < 0 && newnode[first].next != 0) {
+
+					last = first;
+					System.out.println(last);
+					first = newnode[first].next;
+
+				}
+				if (newnode[first].next == 0 && newnode[first].data.compareTo(newnode[p].data) < 0) {
+
+					newnode[p].next = 0;
+					newnode[first].next = p;
+
+				} else if (newnode[first].next == 0) {
+					newnode[p].next = first;
+					newnode[last].next = p;
+					System.out.println(data);
+
+				} else {
+
+					newnode[p].next = first;
+					newnode[last].next = p;
+
+				}
+
+			}
+		}
+
+	}
+
+	public void travarce_rec(int n) {
+		if (newnode[n].next == 0)
+			return;
+System.out.println();
+		int newn = newnode[n].next;
+
+		if (newnode[newn].next == 0) {
+			System.out.println(newnode[newn].data + " -->null");
+
+			return;
+		}
+		System.out.print(newnode[newn].data + " -->");
+		travarce_rec(newn);
+
+	}
+
+	public void trvarse() {
+
+		for (int i = 0; i < newnode.length; i++) {
+			System.out.println(newnode[i]);
+		}
+
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*package dfk;
 
 public class LinkedList<T extends Comparable<T>> {
 
@@ -241,3 +597,4 @@ return curr;
 	}
 
 }
+*/
